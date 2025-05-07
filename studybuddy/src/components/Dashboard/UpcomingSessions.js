c66b7ea6ae2c8a0c84074b06883b37fdd201216e
drrecaj1 <dearrecaj@gmail.com>
2025-05-07 15:49:59 +0200

    fixed a button

--- studybuddy/src/components/Dashboard/UpcomingSessions.js
+++ studybuddy/src/components/Dashboard/UpcomingSessions.js
@@ -1,11 +1,13 @@
 import styles from '@/styles/Dashboard.module.css';
+import Link from "next/link";
 
 export default function UpcomingSessions() {
     return (
         <div className={styles.sessions}>
             <h3>Upcoming Study Sessions</h3>
             <div className={styles.noSessions}>No upcoming sessions.</div>
-            <button className={styles.scheduleBtn}>Schedule a session</button>
+            <button className={styles.scheduleBtn}><Link href="/messages" className={styles.linkInsideButton}>Schedule a session</Link></button>
         </div>
     );
 }
+
